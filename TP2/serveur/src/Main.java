adb9bb1218e1aadf7e3051ce6389bf69275e0928
Arno Peyronon <arno.peyronon@astree-software.fr>
2023-05-23 10:56:21 +0200

    fevbiufbz

--- TP2/serveur/src/Main.java
+++ TP2/serveur/src/Main.java
@@ -1,53 +1,115 @@
-import java.io.*;
-import java.net.ServerSocket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        useHttUrlConnection();
+        useSocket();
+    }
 
-    public static void main(String[] args) {
 
-        int port = 7019;
-        System.out.println("La socket serveur est créé");
-
-        try (
-            ServerSocket serverSocket = new ServerSocket(port);
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out =
-                new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(
-                new InputStreamReader(clientSocket.getInputStream()));
-        ) {
-
-            String inputLine, outputLine;
-            while ((inputLine = in.readLine()) != null) {
-                String[] parts = inputLine.split(":");
-                String action = parts[0];
-                String mot = parts[1];
-                switch (action) {
-                    case "1" -> outputLine = Integer.toString(valeurDuMot(mot));
-                    case "2" -> outputLine = Integer.toString(nombreDeLettres(mot));
-                    default -> {
-                        return;
-                    }
-                }
-                out.println(outputLine);
+    protected static void useHttUrlConnection() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        displayMenu();
+        String value = sc.nextLine();
+        while(value != null) {
+            if(value.equals("1")) {
+                URL url = new URL("http://localhost/iot/TP2/php/index.php");
+                getContentOfPage(url);
             }
-        } catch (IOException e) {
-            System.out.println("Erreur lors de l'écoute sur le port " + port);
-            System.out.println(e.getMessage());
+            else if(value.equals("2")) {
+                String nom = getNom();
+                String age = getAge();
+                URL url = new URL("http://localhost/iot/TP2/php/index.php?nom="+nom+"&age="+age);
+                getContentOfPage(url);
+            } else {
+                wrongChoice();
+            }
+            displayMenu();
+            value = sc.nextLine();
         }
     }
 
-    public static int valeurDuMot(String mot) {
-        return mot.length();
+    public static void useSocket() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        displayMenu();
+        String value = sc.nextLine();
+        String serverHostname = "localhost";
+        String path = "/iot/TP2/php/index.php";
+        int port = 80;
+        String request = "POST " + path;
+
+        while(value != null) {
+            if(value.equals("1")) {
+                Socket socket = new Socket(serverHostname, port);
+                getContentOfPageWithSocket(socket, request, serverHostname);
+            } else      if (value.equals("2")) {
+                String nom = getNom();
+                String age = getAge();
+                request = request + "?nom=" + nom + "&age=" + age;
+                Socket socket = new Socket(serverHostname, port);
+                getContentOfPageWithSocket(socket, request, serverHostname);
+            } else {
+                wrongChoice();
+            }
+            displayMenu();
+            value = sc.nextLine();
+        }
     }
 
-    public static int nombreDeLettres(String mot) {
-        int valeur = 0;
-        for (int i = 0; i < mot.length(); i++) {
-            char c = Character.toLowerCase(mot.charAt(i));
-            valeur += (int) c - 96;
+    public static void getContentOfPageWithSocket(Socket socket, String request,
+        String serverHostname) throws IOException {
+        request = request + " HTTP/1.1\r\n" +
+            "Host: " + serverHostname + "\r\n" +
+            "Connection: close\r\n\r\n";;
+        socket.getOutputStream().write(request.getBytes());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String line;
+        StringBuilder response = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            response.append(line).append("\n");
         }
-        return valeur;
+        socket.close();
+        extractContentOfResult(response);
+    }
+
+    public static void getContentOfPage(URL url) throws IOException {
+        Scanner scNet = new Scanner(url.openStream());
+        StringBuilder sb = new StringBuilder();
+        while(scNet.hasNext()) {
+            sb.append(scNet.next());
+        }
+        extractContentOfResult(sb);
+    }
+
+    public static void extractContentOfResult(StringBuilder response) {
+        String result = response.toString();
+        result= result.replaceAll("<[^>]*>", "\n");
+        System.out.println(result);
+    }
+    public static void wrongChoice() {
+        System.out.println("Choix effectué non valide");
+    }
+
+    public static void displayMenu() {
+        System.out.println("Menu : \n");
+        System.out.println("1 - Aller sur toto.html");
+        System.out.println("2 - Case 2");
+    }
+
+    public static String getNom() {
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Entrez le nom");
+        return sc2.nextLine();
+    }
+
+    public static String getAge() {
+        Scanner sc3 = new Scanner(System.in);
+        System.out.println("Entrez l'age");
+        return sc3.nextLine();
     }
 }
