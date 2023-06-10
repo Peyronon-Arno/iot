69aaf87450887a9c11164e4aaf861170cd8f6210
Arno Peyronon <arno.peyronon@astree-software.fr>
2023-06-10 17:18:45 +0200

    make it works except mdsn

--- TP-PEYRONON-BATOUXAS/PEYRONON-BATOUXAS/PEYRONON-BATOUXAS.ino
+++ TP-PEYRONON-BATOUXAS/PEYRONON-BATOUXAS/PEYRONON-BATOUXAS.ino
@@ -1,4 +1,3 @@
-#include <LittleFS.h>
 
 //littlefs
 // Connectivité wifi
@@ -9,10 +8,12 @@
 // service getTemp / setCredentials
 // mDNS--> Taper http://esp.local au lieu de taper une adresse ip
 
+#include <LittleFS.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include "DHT.h"
 #include "LittleFS.h"
+#include <ESP8266mDNS.h>
 
 #define DHTPIN 2
 #define DHTTYPE DHT11
@@ -21,61 +22,62 @@ DHT dht(DHTPIN, DHTTYPE);
 ESP8266WebServer server(80);
 LittleFSConfig LittleFSConfig;
 FS* fileSystem = &LittleFS;
+
 String ssid;
 String password;
+
 void setup() {
 
   Serial.begin(115200);
   delay(1000);
   dht.begin();
-  WiFi.mode(WIFI_STA);
   LittleFS.setConfig(LittleFSConfig);
   if (!fileSystem->begin()) {
     Serial.println("Erreur lors de l'initialisation de LittleFS");
     return;
   }
   tryConnection();
-  Serial.print("Échec de connexion au réseau");
-  Serial.println(ssid);
-
-  createEndpoint();
-  server.begin();
-
-  server.on("/getTemperature", HTTP_GET, getTemperature);
-  server.on("/getHumidity", HTTP_GET, getHumidity);
-  server.on("/saveCredentials", HTTP_POST, saveCredentials);
 }
 
 void tryConnection() {
   unsigned long startTime = millis();
-  // Tentative de connexion au premier réseau
   readCredentials();
+  WiFi.mode(WIFI_STA);
+
   if (ssid != "" && password != "") {
-    while (millis() - startTime < 10000) {
+    while (millis() - startTime < 30000) {
       Serial.println("On essaie de se connecter");
-      Serial.println(ssid + " " + password);
-      Serial.println(ssid.c_str());
-      Serial.println(password.c_str());
+      ssid.trim();
+      password.trim();
+      Serial.println("...");
       WiFi.begin(ssid.c_str(), password.c_str());
-      Serial.println(".");
       if (WiFi.waitForConnectResult() == WL_CONNECTED) {
         Serial.println("Connexion établie au réseau Wi-Fi");
         Serial.print("Adresse IP: ");
         Serial.println(WiFi.localIP());
+        MDNS.begin(ssid);
+        MDNS.addService("http", "tcp", 80);
         return;
       }
       delay(500);
     }
+  } else {
+    Serial.print("Échec de connexion au réseau");
+    Serial.println(ssid);
+    createEndpoint();
   }
 }
 
 void createEndpoint() {
-  // Si tentative échouée --> création du point d'accès
   WiFi.mode(WIFI_AP);
   WiFi.softAP("ESP8862-" + String(WiFi.macAddress()), "MotDePasse");
   Serial.println("Point d'accès SoftAP créé");
   Serial.println("Adresse IP du point d'accès: ");
   Serial.println(WiFi.softAPIP());
+  server.begin();
+  server.on("/getTemperature", HTTP_GET, getTemperature);
+  server.on("/getHumidity", HTTP_GET, getHumidity);
+  server.on("/saveCredentials", HTTP_POST, saveCredentials);
 }
 
 void getTemperature() {
@@ -88,22 +90,24 @@ void getHumidity() {
 
 void saveCredentials() {
   if (server.args() > 0) {
-    String ssidGet = server.arg("ssid");
-    String pwdGet = server.arg("password");
-    File configFile = fileSystem->open("/credentials.txt", "w");
-    if (!configFile) {
-      Serial.println("Erreur lors de l'ouverture du fichier de configuration");
-      return;
-    }
-    configFile.println(ssidGet);
-    configFile.println(pwdGet);
-    configFile.close();
+    server.arg("ssid");
+    server.arg("password");
   }
+  String ssidGet = "Iphone de Arno";
+  String pwdGet = "arno2002";
+  File configFile = fileSystem->open("/test.txt", "w");
+  if (!configFile) {
+    Serial.println("Erreur lors de l'ouverture du fichier de configuration");
+    return;
+  }
+  configFile.println(ssidGet);
+  configFile.println(pwdGet);
+  configFile.close();
   server.send(200, "text/plain", "Credentials ok");
 }
 
 void readCredentials() {
-  File configFile = fileSystem->open("/credentials.txt", "r");
+  File configFile = fileSystem->open("/test.txt", "r");
   if (!configFile) {
     Serial.println("Erreur lors de l'ouverture du fichier de configuration");
     return;
@@ -120,6 +124,4 @@ void readCredentials() {
 
 void loop() {
   server.handleClient();
-  //Serial.println("Temperature = " + String(dht.readTemperature()) + " °C");
-  //delay(1000);
 }
