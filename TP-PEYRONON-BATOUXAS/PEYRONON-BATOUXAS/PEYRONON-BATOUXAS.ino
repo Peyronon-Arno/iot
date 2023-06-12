273df5bcb654bcdbbd77d4946a248522a64c4bee
leobatouxas <leo.batouxas@gmail.com>
2023-06-12 17:20:42 +0200

    Add averagegtemp

--- TP-PEYRONON-BATOUXAS/PEYRONON-BATOUXAS/PEYRONON-BATOUXAS.ino
+++ TP-PEYRONON-BATOUXAS/PEYRONON-BATOUXAS/PEYRONON-BATOUXAS.ino
@@ -15,6 +15,7 @@
 #include "LittleFS.h"
 #include <ESP8266mDNS.h>
 #include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
 
 #define DHTPIN 2
 #define DHTTYPE DHT11
@@ -28,11 +29,6 @@ String ssid;
 String password;
 
 void setup() {
-  init();
-  tryConnection();
-}
-
-void init() {
   Serial.begin(115200);
   delay(1000);
   dht.begin();
@@ -40,7 +36,13 @@ void init() {
   if (!fileSystem->begin()) {
     Serial.println("Erreur lors de l'initialisation de LittleFS");
     return;
-  }
+  }  
+  server.begin();
+  server.on("/getTemperature", HTTP_GET, getTemperature);
+  server.on("/getHumidity", HTTP_GET, getHumidity);
+  server.on("/saveCredentials", HTTP_POST, saveCredentials);
+  server.on("/averagetemp", HTTP_GET, handleAverageTemperature);
+  tryConnection();
 }
 
 void tryConnection() {
@@ -54,7 +56,8 @@ void tryConnection() {
       ssid.trim();
       password.trim();
       Serial.println("...");
-      WiFi.begin(ssid.c_str(), password.c_str());
+      //WiFi.begin(ssid.c_str(), password.c_str());
+      WiFi.begin("info-licence-a", "pm01web02pm03web04");
       if (WiFi.waitForConnectResult() == WL_CONNECTED) {
         Serial.println("Connexion établie au réseau Wi-Fi");
         Serial.print("Adresse IP: ");
@@ -78,10 +81,6 @@ void createEndpoint() {
   Serial.println("Point d'accès SoftAP créé");
   Serial.println("Adresse IP du point d'accès: ");
   Serial.println(WiFi.softAPIP());
-  server.begin();
-  server.on("/getTemperature", HTTP_GET, getTemperature);
-  server.on("/getHumidity", HTTP_GET, getHumidity);
-  server.on("/saveCredentials", HTTP_POST, saveCredentials);
 }
 
 void getTemperature() {
@@ -105,7 +104,7 @@ void saveCredentials() {
     server.arg("ssid");
     server.arg("password");
   }
-  String ssidGet = "Iphone de Arno";
+  String ssidGet = "";
   String pwdGet = "arno2002";
   File configFile = fileSystem->open("/test.txt", "w");
   if (!configFile) {
@@ -131,6 +130,71 @@ void readCredentials() {
   Serial.println(ssid);
   Serial.print("Password : ");
   Serial.println(password);
+}
+
+float getRemoteTemperature() {
+  WiFiClient client;
+  HTTPClient http;
+
+  http.begin(client, "http://192.168.1.92/temperature");
+
+  int httpResponseCode = http.GET();
+
+  float remoteTemperature = 0.0;  // Valeur par défaut si la requête échoue
+
+  if (httpResponseCode == HTTP_CODE_OK) {
+    Serial.println("response OK")
+    String response = http.getString();
+    Serial.println(response);
+     // Créer un objet JSON
+    DynamicJsonDocument doc(256);
+    
+    // Désérialiser la réponse JSON
+    DeserializationError errordeserializejson = deserializeJson(doc, response);
+    
+    // Vérifier s'il y a eu une erreur lors de la désérialisation
+    if (errordeserializejson) {
+        Serial.print("Erreur lors de la désérialisation : ");
+        Serial.println(error.c_str());
+        return;
+    }
+    
+    // Récupérer la valeur de "temperature"
+    float temperature = doc["temperature"];
+    
+    // Afficher la valeur récupérée
+    Serial.print("Température reçu : ");
+    Serial.println(temperature);
+    remoteTemperature = response.toFloat();
+  }
+
+  http.end();
+
+  return remoteTemperature;
+}
+
+// Calculer la moyenne des températures reçues
+float calculateAverageTemperature() {
+  float remoteTemperature = getRemoteTemperature();
+  return (dht.readTemperature() + remoteTemperature) / 2;
+}
+
+void handleAverageTemperature() {
+  float averageTemperature = calculateAvera
+  geTemperature();
+  
+  StaticJsonDocument<200> jsonDoc;
+  
+  if (isnan(averageTemperature)) {
+    jsonDoc["error"] = "Erreur lors de la lecture de la température !";
+  } else {
+    jsonDoc["temperature"] = temperature;
+  }
+  
+  String response;
+  serializeJson(jsonDoc, response);
+  
+  server.send(200, "application/json", response);
 }
 
 void loop() {
